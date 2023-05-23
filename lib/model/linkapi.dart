62972f1f00f037bc3c45d4a25ea74409b19123ba
Rawan_Abuzir <77068920+rawanabuzir@users.noreply.github.com>
2023-05-23 11:56:44 +0300

    Update linkapi.dart

--- lib/model/linkapi.dart
+++ lib/model/linkapi.dart
@@ -1,5 +1,5 @@
 
-const String linkServerName = "http://192.168.1.179";
+const String linkServerName = "http://192.168.1.5"; // add your ip here 
 // the 10.0.2.2 its the ip for the emulator....
 //auth
 const String linkSignUp = "$linkServerName/PhpRestApiWithFlutter/auth/signup.php";
